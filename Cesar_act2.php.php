e984f0c4107ea9fbb0c42392840fa6e3b99fbdb5
jeproxx01 <123623779+jeproxx01@users.noreply.github.com>
2023-03-28 16:30:08 +0800

    Add files via upload

---
+++ Cesar_act2.php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+
+ $num = 6;
+ 
+ echo "multiplication table of $num <br><br>";
+for ($x = 0; $x <= 10; $x++) {
+  
+   $z = $num * $x;
+   echo "$num * $x = $z";
+   echo '<br><br>';
+   }
+   ?>  
+   
+   
+
+</body>
+</html>
